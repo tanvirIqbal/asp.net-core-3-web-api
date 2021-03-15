d300d874ace09c2fd1233d20d0a0cdc0d6ba8235
Tanvir Iqbal <tanviriqbal4u@live.com>
2021-03-15 22:09:43 +0600

    Repository Pattern implementation

--- CompanyEmployees/CompanyEmployees/Controllers/WeatherForecastController.cs
+++ CompanyEmployees/CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -12,10 +12,12 @@ namespace CompanyEmployees.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private ILoggerManager _logger; 
-        public WeatherForecastController(ILoggerManager logger) 
+        private ILoggerManager _logger;
+        private readonly IRepositoryManager _repository;
+        public WeatherForecastController(ILoggerManager logger, IRepositoryManager repository) 
         { 
-            _logger = logger; 
+            _logger = logger;
+            _repository = repository;
         }
         [HttpGet]
         public IEnumerable<string> Get()
@@ -26,5 +28,11 @@ namespace CompanyEmployees.Controllers
             _logger.LogError("Here is an error message from our values controller."); 
             return new string[] { "value1", "value2" };
         }
+        //[HttpGet] public ActionResult<IEnumerable<string>> Get() 
+        //{ 
+        //    _repository.Company.AnyMethodFromCompanyRepository(); 
+        //    _repository.Employee.AnyMethodFromEmployeeRepository(); 
+        //    return new string[] { "value1", "value2" }; 
+        //}
     }
 }
--- CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,5 +37,8 @@ namespace CompanyEmployees.Extensions
             services.AddDbContext<RepositoryContext>(opts => 
             opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b => 
             b.MigrationsAssembly("CompanyEmployees")));
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services) => 
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
     }
 }
