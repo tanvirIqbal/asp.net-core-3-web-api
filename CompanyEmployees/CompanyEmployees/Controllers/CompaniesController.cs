8caf74c9fcbc4c430ba542d96f1764dbfdbb2f5b
Tanvir Iqbal <tanviriqbal4u@live.com>
2021-03-17 22:16:00 +0600

    GET for single company

--- CompanyEmployees/CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees/Controllers/CompaniesController.cs
@@ -32,5 +32,19 @@ namespace CompanyEmployees.Controllers
             var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
             return Ok(companiesDto);
         }
+
+        [HttpGet("{id}")] 
+        public IActionResult GetCompany(Guid id) 
+        { 
+            var company = _repository.Company.GetCompany(id, trackChanges: false); 
+            if (company == null) 
+            { 
+                _logger.LogInfo($"Company with id: {id} doesn't exist in the database."); 
+                return NotFound(); } 
+            else { 
+                var companyDto = _mapper.Map<CompanyDto>(company); 
+                return Ok(companyDto); 
+            } 
+        }
     }
 }
