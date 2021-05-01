d58675b867b386f98a1d51b12d3cf22e0267313c
Tanvir Iqbal <tanviriqbal4u@live.com>
2021-05-01 15:06:20 +0600

    Create a company

--- CompanyEmployees/CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees/Controllers/CompaniesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Contracts;
 using Entities.DataTransferObjects;
+using Entities.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -33,7 +34,7 @@ namespace CompanyEmployees.Controllers
             return Ok(companiesDto);
         }
 
-        [HttpGet("{id}")] 
+        [HttpGet("{id}", Name = "CompanyById")]
         public IActionResult GetCompany(Guid id) 
         { 
             var company = _repository.Company.GetCompany(id, trackChanges: false); 
@@ -45,6 +46,23 @@ namespace CompanyEmployees.Controllers
                 var companyDto = _mapper.Map<CompanyDto>(company); 
                 return Ok(companyDto); 
             } 
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompany([FromBody] CompanyForCreationDto company)
+        {
+            if (company == null)
+            {
+                _logger.LogError("CompanyForCreationDto object sent from client is null.");
+                return BadRequest("CompanyForCreationDto object is null");
+            }
+            var companyEntity = _mapper.Map<Company>(company); 
+            _repository.Company.CreateCompany(companyEntity); 
+            _repository.Save(); 
+            var companyToReturn = _mapper.Map<CompanyDto>(companyEntity); 
+            return CreatedAtRoute("CompanyById", 
+                new { id = companyToReturn.Id }, 
+                companyToReturn);
         }
     }
 }
--- CompanyEmployees/CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/CompanyEmployees/MappingProfile.cs
@@ -15,6 +15,7 @@ namespace CompanyEmployees
             CreateMap<Company, CompanyDto>()
                 .ForMember(c => c.FullAddress, 
                 opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+            CreateMap<CompanyForCreationDto, Company>();
             CreateMap<Employee, EmployeeDto>();
         }
     }
